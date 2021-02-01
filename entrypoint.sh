4b1143bd4ff00ef332fff771c6424d12e2e5502a
Bill Wang <bill.wang>
2021-02-01 14:26:54 +1100

    feature/#9 - run aws cli v1 and v2 in same image

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
-# Push something on to $PATH depending on the environment
+
+# if environment variable `version` is set to v2, default aws cli version is v2
+# if environment variable `version` is set to v1, not set or set to anything else, except v2, aws cli will be run from /usr/bin, and version is v1
+# you can still run specific version with full path in container.
+# aws cli v1 - installed at /usr/bin
+# aws cli v2 - installed at /usr/local/bin
+
+# adjust $PATH depending on the environment variable "version"
 case "${version}" in
   v1) PATH="/usr/bin:$PATH" ;;
   v2) PATH="/usr/local/bin:$PATH" ;;
